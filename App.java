2d0c8b7fd4ca9d909a13242c7443d5ecf11e9df5
Kovács Dorina <kovacsdorina2@gmail.com>
2022-02-16 12:12:57 +0100

    Program fejlec kesz

--- App.java
+++ App.java
@@ -3,7 +3,8 @@
 	class App {
 	
 	public static void main (String[] args) {
-		
+		System.out.println("Kovacs Dorina, 2022.02.16, Szoft I/N");
+			
 		for (int i=0; i<5; i++){
 			System.out.println(i);
 			
